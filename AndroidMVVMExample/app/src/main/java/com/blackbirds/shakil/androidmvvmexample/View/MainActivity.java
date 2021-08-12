5de092f425818533fdd22500485bf996dc5d2375
android_shakil <shakil!@#$1234>
2021-08-12 23:25:35 +0600

    View Model example

--- AndroidMVVMExample/app/src/main/java/com/blackbirds/shakil/androidmvvmexample/View/MainActivity.java
+++ AndroidMVVMExample/app/src/main/java/com/blackbirds/shakil/androidmvvmexample/View/MainActivity.java
@@ -3,23 +3,27 @@ package com.blackbirds.shakil.androidmvvmexample.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.blackbirds.shakil.androidmvvmexample.R;
+import com.blackbirds.shakil.androidmvvmexample.ViewModel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     AppCompatEditText edtNumberOne, edtNumberTwo;
     AppCompatButton btnAdd;
     TextView txtResult;
+    MainActivityViewModel mainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
         edtNumberOne = findViewById(R.id.edtNumberOne);
         edtNumberTwo = findViewById(R.id.edtNumberTwo);
@@ -27,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         txtResult = findViewById(R.id.txtResult);
 
         btnAdd.setOnClickListener(v -> {
-
+            String resultText = mainActivityViewModel.addFunction(edtNumberOne.getText().toString(),
+                    edtNumberTwo.getText().toString());
+            txtResult.setText(resultText);
         });
     }
 }
--- AndroidMVVMExample/app/src/main/java/com/blackbirds/shakil/androidmvvmexample/ViewModel/MainActivityViewModel.java
+++ AndroidMVVMExample/app/src/main/java/com/blackbirds/shakil/androidmvvmexample/ViewModel/MainActivityViewModel.java
@@ -1,2 +1,13 @@
-package com.blackbirds.shakil.androidmvvmexample.ViewModel;public class MainActivityViewModel {
+package com.blackbirds.shakil.androidmvvmexample.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    public String addFunction(String numberOne, String numberTwo){
+        int numOne = Integer.parseInt(numberOne);
+        int numTwo = Integer.parseInt(numberTwo);
+        int result = numOne + numTwo;
+
+        return String.valueOf(result);
+    }
 }
